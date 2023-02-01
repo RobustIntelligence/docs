21e523a4b8b350a33f8b1b5f782d27d06ac84357
palecur <paul@robustintelligence.com>
2023-02-01 13:31:04 -0800

    updates for test DB ancillary information

--- html/_static/main.js
+++ html/_static/main.js
@@ -1,3 +1,16 @@
+/* Adds custom search builder, positions elements */
+
 $(document).ready( function () {
-    $('table.datatable').DataTable();
+    $('table.datatable').DataTable({
+      searchBuilder: {
+        columns: [0,1,2,6]
+      },
+      columnDefs: [{
+        targets: [0, 1, 2, 6,],
+        searchBuilder: {
+            defaultCondition: '='
+        }
+    }],
+      dom: '<"top"Qrt><"bottom"flip><"clear">',
+    });
 } );
